85fe8cd7776fe3bcf0b7849b70d0af14f176a408
Pavel <pavel@p-p.name>
2024-10-21 22:40:34 +0300

    + tg_cb_flow_list basic content

--- migrations/3_tg_cb_flow_schema.up.sql
+++ migrations/3_tg_cb_flow_schema.up.sql
@@ -13,3 +13,7 @@ CREATE TABLE tg_cb_flow (
     row_order INTEGER NOT NULL,
     added_timestamp BIGINT NOT NULL DEFAULT (strftime('%s', 'now'))
 );
+
+INSERT INTO tg_cb_flow_list (id, name, row_order, added_timestamp) VALUES (1, 'Shift start', 10, 1729539190);
+INSERT INTO tg_cb_flow_list (id, name, row_order, added_timestamp) VALUES (2, 'Sale', 20, 1729539318);
+INSERT INTO tg_cb_flow_list (id, name, row_order, added_timestamp) VALUES (3, 'Shift close', 30, 1729539433);
