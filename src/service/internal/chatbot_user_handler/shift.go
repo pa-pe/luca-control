21a363de52501b21bcd121b639cbf601d5156da1
Pavel <pavel@p-p.name>
2024-10-24 18:45:06 +0300

    + first edition of package chatbot_user_handler

---
+++ src/service/internal/chatbot_user_handler/shift.go
@@ -0,0 +1,65 @@
+package chatbot_user_handler
+
+import (
+	"fmt"
+	"github.com/pa-pe/luca-control/src/storage"
+	"github.com/pa-pe/luca-control/src/storage/model"
+	"log"
+)
+
+var cbServerErr = "oops, chatbot server error"
+
+var functions = map[string]func(telegramStorage storage.ITelegram, tgUser *model.TgUser, msg string) (string, string){
+	"getLocationsKeyboard":     getLocationsKeyboard,
+	"handleUserChooseLocation": handleUserChooseLocation,
+}
+
+func Handle(telegramStorage storage.ITelegram, functionName string, tgUser *model.TgUser, msg string) (string, string) {
+	if function, exists := functions[functionName]; exists {
+		return function(telegramStorage, tgUser, msg)
+	} else {
+		log.Printf("chatbot_user_handler: Function '%s' not found!", functionName)
+	}
+	return "", ""
+}
+
+func HandleServerError() (string, string) {
+	return cbServerErr, ""
+}
+
+func getLocationsKeyboard(telegramStorage storage.ITelegram, tgUser *model.TgUser, msg string) (string, string) {
+	_ = tgUser
+
+	srvsLocationList, err := telegramStorage.GetSrvsLocationList("")
+	if err != nil {
+		log.Printf("chatbot_user_handler: GetSrvsLocationList failed: %v", err)
+		return cbServerErr, ""
+	}
+
+	keyboard := ""
+	for _, record := range srvsLocationList {
+		if keyboard != "" {
+			keyboard += "|"
+		}
+		keyboard += record.Name
+	}
+
+	return msg, keyboard
+}
+
+func handleUserChooseLocation(telegramStorage storage.ITelegram, tgUser *model.TgUser, msg string) (string, string) {
+	_ = tgUser
+
+	srvsLocationList, err := telegramStorage.GetSrvsLocationList(fmt.Sprintf(`name = "%s"`, msg))
+	if err != nil {
+		log.Printf("chatbot_user_handler: GetSrvsLocationList failed: %v", err)
+		return cbServerErr, ""
+	}
+
+	if len(srvsLocationList) == 0 {
+		return "Please tap locations button from menu", "func:getLocationsKeyboard"
+	}
+
+	// return empty if handler pass userdata
+	return "", ""
+}
